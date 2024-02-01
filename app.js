a4fdffcf7afe51f99f33a6a1ffa1310de7621abb
prasanthj2023 <prasanth.j@kalvium.community>
2024-02-01 10:57:05 +0530

    Updated

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// const container = document.getElementById('react-container');
+
+// ReactDOM.render("Hello! Welcome to React",container);
+
+
 // const Container = () =>{
 //     return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
 //        React.createElement(`div`,null,`Let's rock and roll`)
@@ -6,13 +11,10 @@
 
 // const container = document.getElementById('react-container');
 // ReactDOM.render(React.createElement(Container),container);
-// ReactDOM.render("Hello! Welcome to React",container);
-
 
 // class ReactContainer extends React.Component{
-//     // constructor - to initialize the state 
-  
-//     // render method to render the react dom 
+
+
 //       render(){
 //           return React.createElement(`div`,null,`Hey Kalvians`,
 //           React.createElement(`div`,null,`Let's rock and roll with classes`)
@@ -20,35 +22,20 @@
 //       }
     
 //   }
-// const container = document.getElementById('react-container');
-// ReactDOM.render(React.createElement(ReactContainer),container);
+//   const container = document.getElementById('react-container');
+//   ReactDOM.render(React.createElement(ReactContainer),container);
 
-// class ReactContainer extends React.Component{
-//     // JSX tags
-//     // JSX there should be one parent element
-//       render(){
-//           return (
-//           <div>Hello! Welcome to Kalvium   
-//              <div>This is babel</div>
-//          </div> )
-//       };  
-//   }
-
-// const container = document.getElementById('react-container');
-// // Direclty pass the component to render
-// ReactDOM.render(<ReactContainer/>,container);    
 
 class ReactContainer extends React.Component{
-  // JSX tags
-  // JSX there should be one parent element
-    render(){
-        return (
-        <div>Hello! Welcome to Kalvium   
-           <div>This is babel</div>
-       </div> )
-    };  
+
+  render(){
+      return (
+      <div>Hello! Welcome to Kalvium   
+        <div>Lets Rock and Roll - This is babel</div>
+     </div> )
+  };  
 }
 
 const container = document.getElementById('react-container');
-// Direclty pass the component to render
+
 ReactDOM.render(<ReactContainer/>,container);    
